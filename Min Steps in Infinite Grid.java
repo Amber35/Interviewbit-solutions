73c03cd845ee2ba4c924e663a3ae39ba476adb39
Amber Pandit <amberpandit35@gmail.com>
2017-09-27 14:53:49 +0530

    Create Min Steps in Infinite Grid.java

---
+++ Min Steps in Infinite Grid.java
@@ -0,0 +1,18 @@
+public class Solution {
+    // X and Y co-ordinates of the points in order.
+    // Each point is represented by (X.get(i), Y.get(i))
+    public int coverPoints(ArrayList<Integer> X, ArrayList<Integer> Y) {
+        int i=0,j=0,xcost,ycost,cost=0;
+        while(X.size()>i+1 && Y.size()>j+1)
+        {
+           xcost=Math.abs(X.get(i)-X.get(i+1));
+            ycost=Math.abs(Y.get(j)-Y.get(j+1));
+           // System.out.println("x "+xcost);
+            //System.out.println("y "+ycost);
+            i++;j++;
+          cost=cost+Math.max(xcost,ycost);
+            
+        }
+        return cost;
+    }
+}
